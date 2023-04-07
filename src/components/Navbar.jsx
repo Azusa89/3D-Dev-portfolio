ff948bd95120e3bf148e77659d85ea447d59f10d
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2023-04-07 11:18:36 +0900

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,10 +30,23 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
+          <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Kaho Cheng
           </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
